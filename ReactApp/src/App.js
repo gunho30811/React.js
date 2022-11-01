e93f54930bed979b6edb1aef357c60733f71b30c
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-11-01 17:02:07 +0900

    2022-11-01 17:01
    React.js 정리 및 6.4# 끝 7# 가야함.

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from 'react';
+
 function App() {
-   return (
-      <div>
-         <h1>Welcome back!</h1>
-      </div>
-   );
+	const [counter, setValue] = useState(0);
+	const [keyword, setKeyword] = useState('');
+	const onClick = () => setValue(prev => prev + 1);
+	const onChange = e => setKeyword(e.target.value);
+	useEffect(() => {
+		if (counter !== 0) console.log('counter', counter);
+	}, [counter]);
+	useEffect(() => {
+		if (keyword !== '' && keyword.length > 5) {
+			console.log('SEARCH FOR', keyword);
+		}
+	}, [keyword]);
+	useEffect(() => {
+		console.log('CALL THE API....');
+	}, []);
+
+	return (
+		<div>
+			<input
+				value={keyword}
+				onChange={onChange}
+				type='text'
+				placeholder='Search here...'
+			/>
+			<h1>{counter}</h1>
+			<button onClick={onClick}>Click me</button>
+		</div>
+	);
 }
 
 export default App;
