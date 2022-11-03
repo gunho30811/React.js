fff74d6209ac9f8749d46d468b7a047331851063
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-11-03 17:48:52 +0900

    2022-11-03 17:48
    지금 해야 할 것은 coin.symbol을 불러오는 것.

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
 
-function WonToDollar() {
+function App() {
+	const [loading, setLoading] = useState(true);
+	const [coins, setcoins] = useState([]);
+	const [Convert, setConvert] = useState(0);
 	const [Won, setWon] = useState(0);
 	const [Dollar, setDollar] = useState(0);
+	const kkk = Won / (Convert * 1418.29);
 	const onChange = e => {
 		if (e.target.id === 'Won') {
 			setWon(e.target.value);
@@ -13,30 +17,10 @@ function WonToDollar() {
 		}
 	};
 
-	return (
-		<div>
-			<h3>How Much Do you have?</h3>
-			<input
-				id='Won'
-				type='text'
-				value={Won}
-				onChange={onChange}></input>
-			<label> &#8361; </label>
-			<input
-				id='Dollar'
-				type='text'
-				value={Dollar}
-				onChange={onChange}></input>
-			<label> &#36;</label>
-		</div>
-	);
-}
-
-function App() {
-	const [loading, setLoading] = useState(true);
-	const [coins, setcoins] = useState([]);
 	const showValue = e => {
+		console.log(document.getElementById(``));
 		console.log(e.target.value);
+		setConvert(e.target.value);
 	};
 	// const [convert, setconvert] = useState(0);
 	useEffect(() => {
@@ -50,17 +34,38 @@ function App() {
 	return (
 		<div>
 			<h1>The Coins! ({loading ? '' : `${coins.length}`})</h1>
-			<WonToDollar />
+			<h3>How Much Do you have?</h3>
+			<h3>i want to see my opinion</h3>
+			<input
+				id='Won'
+				type='text'
+				value={Won}
+				onChange={onChange}></input>
+			<label> &#8361; </label>
+			<input
+				id='Dollar'
+				type='text'
+				value={Dollar}
+				onChange={onChange}></input>
+			<label> &#36;</label>
 			<h3>Convert Price</h3>
-			<input type='text'></input>
-			<h3>Current Prices</h3>
+			<input
+				type='text'
+				value={kkk}
+				readOnly></input>
+			<h3>Current Coin Prices </h3>
 			{loading ? (
 				<strong>Loading...</strong>
 			) : (
-				<select onChange={showValue}>
+				<select
+					id='Coin'
+					onChange={showValue}>
 					{coins.map(coin => (
-						<option key={coin.id}>
-							{coin.name} ({coin.symbol}) : {coin.quotes.USD.price}price
+						<option
+							key={coin.id}
+							value={coin.quotes.USD.price}
+							id={coin.symbol}>
+							{coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
 						</option>
 					))}
 				</select>
