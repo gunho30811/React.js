d07cdfb8a02b204782186f7957731c9a2a518e0a
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-12-19 16:53:41 +0900

    2022-12-19 16:54
    TypeScript

--- react-type-master/src/App.tsx
+++ react-type-master/src/App.tsx
@@ -1,15 +1,17 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Circle from './Circle';
 function App() {
+	const Container = styled.div`
+		background-color: ${props => props.theme.bgColor};
+	`;
+	const H1 = styled.h1`
+		color: ${props => props.theme.textColor};
+	`;
 	return (
-		<div>
-			<Circle bgColo='teal' />
-			<Circle
-				borderColor='yellow'
-				bgColo='tomato'
-				text='hi text on'
-			/>
-		</div>
+		<Container>
+			<H1>protected</H1>
+		</Container>
 	);
 }
 
